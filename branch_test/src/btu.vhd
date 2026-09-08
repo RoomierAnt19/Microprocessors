a7801ade15d55b30cd56676000d3c8f818fcb950
Micah Halbersma <Micah.halbersma@gmail.com>
2026-09-08 12:09:10 -0600

    folder clean up for organization

--- branch_test/src/btu.vhd
+++ branch_test/src/btu.vhd
@@ -3,10 +3,10 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 entity btu is
-  generic( N: positive := 32);
+  generic( XLEN: positive := 32);
   port (
-    RS1 : in std_logic_vector(N-1 downto 0);
-    RS2 : in std_logic_vector(N-1 downto 0);
+    RS1 : in std_logic_vector(XLEN-1 downto 0);
+    RS2 : in std_logic_vector(XLEN-1 downto 0);
     cond : in std_logic_vector(2 downto 0);
     enable : in std_logic;
     take_branch : out std_logic
@@ -14,24 +14,24 @@ entity btu is
 end entity btu;
 
 architecture rtl of btu is
-  signal adout: std_logic_vector(N-1 downto 0);
+  signal adout: std_logic_vector(XLEN-1 downto 0);
   signal co, ovf, lt, ltu, eq, output: std_logic;
   signal outcode: std_logic_vector(1 downto 0);
 
 
 begin
   compare : entity work.Adder_Subtractor(Behavioral)
-    generic map (N => N)
+    generic map (N => XLEN)
     Port map(
               a => RS1,
-              b => RS2S2,
+              b => RS2,
               add_sub => '1',
               r => adout,
               carry_out => co,
               overflow => ovf
             );
 
-  lt <= adout(N-1) xor ovf;
+  lt <= adout(XLEN-1) xor ovf;
   ltu <= not co;
   eq <= '1' when unsigned(adout) = 0 else '0';
 
